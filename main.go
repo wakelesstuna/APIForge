53d1b327c8202450b4f1fd460c11cdd0667fb429
Oscar Steen Forss <oscar.forss.1@gmail.com>
2023-10-24 21:45:27 +0200

    removed custom assetsloader

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"embed"
-	"fmt"
-	"net/http"
-	"os"
-	"strings"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/logger"
@@ -16,27 +12,6 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-type FileLoader struct {
-	http.Handler
-}
-
-func NewFileLoader() *FileLoader {
-	return &FileLoader{}
-}
-
-func (h *FileLoader) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	var err error
-	requestedFilename := strings.TrimPrefix(req.URL.Path, "/")
-	println("Requesting file:", requestedFilename)
-	fileData, err := os.ReadFile(requestedFilename)
-	if err != nil {
-		res.WriteHeader(http.StatusBadRequest)
-		res.Write([]byte(fmt.Sprintf("Could not load file %s", requestedFilename)))
-	}
-
-	res.Write(fileData)
-}
-
 func main() {
 	app := NewApp()
 
@@ -46,8 +21,7 @@ func main() {
 		Height:   768,
 		LogLevel: logger.DEBUG,
 		AssetServer: &assetserver.Options{
-			Assets:  assets,
-			Handler: NewFileLoader(),
+			Assets: assets,
 		},
 		Debug: options.Debug{
 			OpenInspectorOnStartup: true,
